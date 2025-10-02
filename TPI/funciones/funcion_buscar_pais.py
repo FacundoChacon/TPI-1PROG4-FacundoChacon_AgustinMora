8d9fb293be0c4e4fcfeebfd1a13aab02a3b0edbb
unknown <agustinmora2006@gmail.com>
2025-10-02 17:03:44 -0300

    Añadida la función de la opción 1 del menú

--- TPI/funciones/funcion_buscar_pais.py
+++ TPI/funciones/funcion_buscar_pais.py
@@ -1,5 +1,15 @@
+from paises_info import *
 def funcion_buscar_pais(lista_paises, pais_a_buscar):
+    contador = 0
     pais = pais_a_buscar.lower()
+    if pais in lista_paises:
+        indice = lista_paises.index(pais)
+        if lista_paises[indice]:
+            return f"El país '{lista_paises[indice].title()}' fue encontrado en la lista."
     for i in range(0, len(lista_paises)):
         if pais in lista_paises[i]:
             return f"El país '{lista_paises[i].title()}' fue encontrado en la lista."
+        else:
+            contador+=1
+            if contador>=(len(paises_info)):
+                return f'No se encontró "{pais_a_buscar}", por favor revise que haya introducido bien el texto.'
