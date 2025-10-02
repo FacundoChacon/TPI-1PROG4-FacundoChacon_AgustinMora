c9b047fa0a876b21d5f39868ab30e481a0aeb121
unknown <agustinmora2006@gmail.com>
2025-10-02 18:25:57 -0300

    Añadida la segunda opción para filtrar paises (por población)

--- TPI/funciones/funcion_filtrar_paises.py
+++ TPI/funciones/funcion_filtrar_paises.py
@@ -1,5 +1,6 @@
 from paises_info import *
-from selecciones.seleccion_filtrar_paises import *
+from selecciones.seleccion_filtrar_paises_continente import *
+from selecciones.seleccion_filtrar_paises_poblacion import *
 def funcion_filtrar_paises(seleccion):
 
 # Filtrar países por:
@@ -10,10 +11,10 @@ def funcion_filtrar_paises(seleccion):
         match seleccion:
             case 1:
                 seleccion = int(input("\nElige una opción:\n1) África\n2) América\n3) Asia\n4) Europa\n5) Oceanía     |"))
-                seleccion_filtrar_paises(seleccion)
-                
+                seleccion_filtrar_paises_continente(seleccion)
             case 2:
-                pass
+                seleccion = int(input("Eliga una opción:\n1) Menos de 1 Millón\n2) Entre 1-10 Millones\n3) Más de 10 Millones   |"))
+                seleccion_filtrar_paises_poblacion(seleccion)
             case 3:
                 pass
         
