6ffd9d8bd6b2a28eff7f7898e5871a446bf6b768
unknown <agustinmora2006@gmail.com>
2025-10-03 17:14:12 -0300

    Se añadió la opción 3 (superficie)

--- TPI/funciones/funcion_filtrar_paises.py
+++ TPI/funciones/funcion_filtrar_paises.py
@@ -1,6 +1,7 @@
 from paises_info import *
 from selecciones.seleccion_filtrar_paises_continente import *
 from selecciones.seleccion_filtrar_paises_poblacion import *
+from selecciones.seleccion_filtrar_paises_superficie import *
 def funcion_filtrar_paises(seleccion):
 
 # Filtrar países por:
@@ -16,7 +17,8 @@ def funcion_filtrar_paises(seleccion):
                 seleccion = int(input("Eliga una opción:\n1) Menos de 1 Millón\n2) Entre 1-10 Millones\n3) Más de 10 Millones   |"))
                 seleccion_filtrar_paises_poblacion(seleccion)
             case 3:
-                pass
+                seleccion = int(input("\nEliga una opción:\n1) Menos de 500km\n2) Entre 500-1.000km\n3) Entre 1.000-10.000km\n4) Entre 10.000-50.000km\n5) Entre 50.000-100.000km\n6) Entre 100.000-200.000km\n7) Entre 200.000-500.000km\n8) Entre 500.000-1.000.000km\n9) Entre 1.000.000-3.000.000km\n10) Más de 3.000.000km   |"))
+                funcion_filtrar_paises_superficie(seleccion)
         
     else:
         print("MAL")
