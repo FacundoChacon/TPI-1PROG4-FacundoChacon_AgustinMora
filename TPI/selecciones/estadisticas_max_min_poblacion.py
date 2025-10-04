c76b7900399ceea27019768d69b8a7fbf7b2e0b5
FacundoChacon <chaconfacu2006@gmail.com>
2025-10-04 20:41:33 -0300

    Se añadio la subfuncion para mostrar cual es el pais con mas poblacion tiene y cual es el que menos poblacion tiene

---
+++ TPI/selecciones/estadisticas_max_min_poblacion.py
@@ -0,0 +1,17 @@
+from paises_info import *
+poblacion = []
+nombre_paises = []
+def max_min_poblacion():
+    for i in range(0,len(paises_info)):
+                poblacion.append(paises_info[i].get("poblacion"))
+                nombre_paises.append(paises_info[i].get("nombre"))
+    for i in range(0,len(poblacion)-1):
+        for j in range(i+1,len(poblacion)):
+            if poblacion[i]>poblacion[j]:
+                poblacion[i], poblacion[j] = poblacion[i], poblacion[j]
+                nombre_paises[i], nombre_paises[j] = nombre_paises[i], nombre_paises[j]
+            else:
+                poblacion[i], poblacion[j] = poblacion[j], poblacion[i]
+                nombre_paises[i], nombre_paises[j] = nombre_paises[j], nombre_paises[i]
+    print(f"El pais con mas poblacion es '{nombre_paises[0]}' y tiene {poblacion[0]}")
+    print(f"El pais con mas poblacion es '{nombre_paises[len(nombre_paises)-1]}' y tiene {poblacion[len(poblacion)-1]}")
