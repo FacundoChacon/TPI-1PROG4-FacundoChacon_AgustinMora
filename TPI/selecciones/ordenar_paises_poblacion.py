a954ddfafae8a68d92d6a33b85399e34e6d31de6
FacundoChacon <chaconfacu2006@gmail.com>
2025-10-04 20:32:14 -0300

    Se ha añadido la funcion para ordenar la poblacion de paises

--- TPI/selecciones/ordenar_paises_poblacion.py
+++ TPI/selecciones/ordenar_paises_poblacion.py
@@ -1,5 +1,5 @@
 from paises_info import *
-def ordenar_paises_poblacion():
+def ordenar_paises_poblacion(): #Ordena los países por población de mayor a menor, mostrando su nombre y cantidad de habitantes
     habitantes_paises = []
     nombre_paises = []
     for i in range(0,len(paises_info)):
