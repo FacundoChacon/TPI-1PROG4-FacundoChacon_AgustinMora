c52df1cc5896e4603a2d1e48055d41a307e3a36e
FacundoChacon <chaconfacu2006@gmail.com>
2025-10-04 20:36:13 -0300

    Se añdio la funcion para ordenar los paises segun la superficiecon las opciones de mayor a menor o viceversa a elegir por el usuario

--- TPI/selecciones/ordenar_paises_superficie.py
+++ TPI/selecciones/ordenar_paises_superficie.py
@@ -1,17 +1,34 @@
 from paises_info import *
-def ordenar_paises_superficie():
+def ordenar_paises_superficie(): #Ordena los países por superficie de mayor a menor, mostrando su nombre y cantidad de Km²
     superficie_paises = []
     nombre_paises = []
-    for i in range(0,len(paises_info)):
-        superficie_paises.append(paises_info[i].get("superficie"))
-        nombre_paises.append(paises_info[i].get("nombre"))
-    for i in range(0,len(superficie_paises)-1):
-        for j in range(i+1,len(superficie_paises)):
-            if superficie_paises[i]>superficie_paises[j]:
-                superficie_paises[i], superficie_paises[j] = superficie_paises[i], superficie_paises[j]
-                nombre_paises[i], nombre_paises[j] = nombre_paises[i], nombre_paises[j]
-            else:
-                superficie_paises[i], superficie_paises[j] = superficie_paises[j], superficie_paises[i]
-                nombre_paises[i], nombre_paises[j] = nombre_paises[j], nombre_paises[i]
-    for i in range(0,len(superficie_paises)):
-        print(f"El país {nombre_paises[i]} tiene {superficie_paises[i]} Km² de superficie")
+    eleccion = int(input("Elija 1 para ordenar de mayor a menor o 2 para ordenar de menor a mayor: "))
+    match eleccion:
+        case 1:
+            for i in range(0,len(paises_info)):
+                superficie_paises.append(paises_info[i].get("superficie"))
+                nombre_paises.append(paises_info[i].get("nombre"))
+            for i in range(0,len(superficie_paises)-1):
+                for j in range(i+1,len(superficie_paises)):
+                    if superficie_paises[i]>superficie_paises[j]:
+                        superficie_paises[i], superficie_paises[j] = superficie_paises[i], superficie_paises[j]
+                        nombre_paises[i], nombre_paises[j] = nombre_paises[i], nombre_paises[j]
+                    else:
+                        superficie_paises[i], superficie_paises[j] = superficie_paises[j], superficie_paises[i]
+                        nombre_paises[i], nombre_paises[j] = nombre_paises[j], nombre_paises[i]
+            for i in range(0,len(superficie_paises)):
+                print(f"El país {nombre_paises[i]} tiene {superficie_paises[i]} Km² de superficie")
+        case 2:
+            for i in range(0,len(paises_info)):
+                superficie_paises.append(paises_info[i].get("superficie"))
+                nombre_paises.append(paises_info[i].get("nombre"))
+            for i in range(0,len(superficie_paises)-1):
+                for j in range(i+1,len(superficie_paises)):
+                    if superficie_paises[i]>superficie_paises[j]:
+                        superficie_paises[i], superficie_paises[j] = superficie_paises[j], superficie_paises[i]
+                        nombre_paises[i], nombre_paises[j] = nombre_paises[j], nombre_paises[i]
+                    else:
+                        superficie_paises[i], superficie_paises[j] = superficie_paises[i], superficie_paises[j]
+                        nombre_paises[i], nombre_paises[j] = nombre_paises[i], nombre_paises[j]
+            for i in range(0,len(superficie_paises)):
+                print(f"El país {nombre_paises[i]} tiene {superficie_paises[i]} Km² de superficie")
