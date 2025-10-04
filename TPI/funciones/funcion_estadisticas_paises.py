9ebc7e143c98155a063b8560578398d786f8af25
FacundoChacon <chaconfacu2006@gmail.com>
2025-10-04 20:38:34 -0300

    Se añadio la funcion general para calcular estadisticas de paises segun lo que ingrese el usuario

---
+++ TPI/funciones/funcion_estadisticas_paises.py
@@ -0,0 +1,18 @@
+from paises_info import *
+from selecciones.estadisticas_max_min_poblacion import *
+from selecciones.estadisticas_paises_por_continente import *
+from selecciones.estadisticas_promedio_poblacion import *
+from selecciones.estadisticas_promedio_superficie import *
+def estadisticas(seleccion):
+    match seleccion:
+        case 1:
+            max_min_poblacion()
+        case 2:
+            pais = input("Ingrese el pais que desee buscar: \n").title()
+            promedio(pais)
+        case 3:
+            promedio_superficie()
+        case 4:
+            paises_continente()
+        case _:
+            print("Se esperaba una de las opciones indicadas, intentelo de nuevo")
